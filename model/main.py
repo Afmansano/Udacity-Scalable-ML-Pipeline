b99b8a29d5a209a5a123a9281e7196efa22ad04b
afmansano <afmansano@gmail.com>
2022-03-20 21:51:51 -0300

    config dvc for heroku

--- model/main.py
+++ model/main.py
@@ -3,6 +3,14 @@
 import argparse
 import train_model
 import compute_scores
+import os
+
+
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
 
 
 def go(args):
