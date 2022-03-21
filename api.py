fc4beb6a7248a53fb62bd52b052b0d444c08b254
afmansano <afmansano@gmail.com>
2022-03-21 11:54:07 -0300

    remove create remote

--- api.py
+++ api.py
@@ -31,10 +31,6 @@ app = FastAPI()
 
 if "DYNO" in os.environ and os.path.isdir(".dvc"):
     os.system("dvc config core.no_scm true")
-    try:
-        os.system("dvc remote add -d awsremote s3://udacity-afmansano")
-    except:
-        pass
     if os.system("dvc pull") != 0:
         exit("dvc pull failed")
     os.system("rm -r .dvc .apt/usr/lib/dvc")
