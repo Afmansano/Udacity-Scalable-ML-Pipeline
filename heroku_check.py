6a5eebb12a0bb9319b34fe03a96bb09bbca6c23c
afmansano <afmansano@gmail.com>
2022-03-21 15:46:45 -0300

    add test_above

--- heroku_check.py
+++ heroku_check.py
@@ -3,7 +3,7 @@ Heroku Api test script
 """
 import requests
 
-def test_post():
+def test_post_bellow():
     person = {
         'age': 39,
         'workclass': 'State-gov',
@@ -27,14 +27,40 @@ def test_post():
     print("POST Response body: %s" % r.json())
 
 
-def test_get():
-    r = requests.get('https://afmansanomlpipeline.herokuapp.com/')
+
+def test_post_above():
+    person = {
+        'age': 30,
+        'workclass': 'State-gov',
+        'fnlgt': 141297,
+        'education': 'Bachelors',
+        'education_num': 13, 
+        'maritial_status': 'Married-civ-spouse', 
+        'ocuupation': 'Prof-specialty', 
+        'relationship': 'Husband', 
+        'race': 'Asian-Pac-Islander', 
+        'sex': 'Male', 
+        'capital_gain': 0, 
+        'capital_loss': 0, 
+        'hours_per_week': 40, 
+        'native_country': 'India'       
+    }
+    r = requests.post('https://afmansanomlpipeline.herokuapp.com/', json=person)
     assert r.status_code == 200
 
     print("POST Response code: %s" % r.status_code)
     print("POST Response body: %s" % r.json())
 
 
+def test_get():
+    r = requests.get('https://afmansanomlpipeline.herokuapp.com/')
+    assert r.status_code == 200
+
+    print("GET Response code: %s" % r.status_code)
+    print("GET Response body: %s" % r.json())
+
+
 if __name__ == '__main__':
     test_get()
-    test_post()
+    test_post_bellow()
+    test_post_above()
