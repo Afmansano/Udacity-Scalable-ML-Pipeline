6c165e959345264bfa136cbd22174af47cf08f92
afmansano <afmansano@gmail.com>
2022-03-20 23:41:42 -0300

    change root folder

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ app = FastAPI()
 
 if "DYNO" in os.environ and os.path.isdir(".dvc"):
     os.system("dvc config core.no_scm true")
-    s.system("dvc remote add -d awsremote s3://udacity-afmansano")
+    os.system("dvc remote add -d awsremote s3://udacity-afmansano")
     if os.system("dvc pull") != 0:
         exit("dvc pull failed")
     os.system("rm -r .dvc .apt/usr/lib/dvc")
